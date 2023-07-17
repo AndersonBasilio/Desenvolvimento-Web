2448b9d871e4d015be5764052987463246a79b4d
Anderson Basilio <andersonylde32@gmail.com>
2023-07-17 16:36:52 -0300

    Create aula_javascript02_script.js

---
+++ aula_javascript02_script.js
@@ -0,0 +1,13 @@
+        //string
+        var texto = 'Curso de Javascript'
+        
+        //number
+        var numeroInteiro = -7
+        var numero_fracionado = 107.89
+
+        //boolean teste condicionais
+        var teste = false // true -> verdadeiro / false -> falso
+
+        //alert(numero_fracionado) // abrir um dialogo
+        //document.write(teste)
+        console.log(numeroInteiro) 
