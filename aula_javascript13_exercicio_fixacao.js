0155f572f8f22c2f8abc95797e5b138f3809decd
Anderson Basilio <andersonylde32@gmail.com>
2023-11-23 19:33:58 -0300

    Update aula_javascript13_exercicio_fixacao.js

--- aula_javascript13_exercicio_fixacao.js
+++ aula_javascript13_exercicio_fixacao.js
@@ -10,18 +10,25 @@ var imc = peso / (altura_cm * altura_cm)
 
 if (imc < 16){
     document.write(nome + ' possui índice de massa corporal igual a ' + imc + ' Muito abaixo do peso, muito grave.')
+
 } else if (imc >= 16 && imc < 17){
     document.write(nome + ' possui índice de massa corporal igual a ' + imc + ' Abaixo do peso, grave.')
+
 } else if (imc >= 17 && imc < 18.49){
     document.write(nome + ' possui índice de massa corporal igual a ' + imc + ' Baixo peso.')
+
 } else if (imc > 18.5 &&  imc < 24.99){
     document.write(nome + ' possui índice de massa corporal igual a ' + imc + ' Peso Normal')
+
 } else if (imc > 25 && imc < 30){
     document.write(nome + ' possui índice de massa corporal igual a ' + imc + ' Sobrepeso')
+
 } else if (imc > 30 && imc < 35){
     document.write(nome + ' possui índice de massa corporal igual a ' + imc + ' Obesidade grau I')
+
 } else if (imc > 35 && imc < 40 ){
     document.write(nome + ' possui índice de massa corporal igual a ' + imc + ' Obesidade grau II')
+    
 } else {
     document.write(nome + ' possui índice de massa corporal igual a ' + imc + ' Obesidade grau III')
 }
