18504e4e1e0fc391d1dca45697051e970667f098
Anderson Basilio <andersonylde32@gmail.com>
2023-12-15 19:30:57 -0300

    Update aula_javascript25_eventos_formulario.js

--- aula_javascript25_eventos_formulario.js
+++ aula_javascript25_eventos_formulario.js
@@ -1,12 +1,12 @@
 function acao1(){
-    alert('Recebeu o foco.')
+    alert('Recebeu o foco.');
 }
 
 function acao2(){
-    alert('Perdeu o foco.')
+    alert('Perdeu o foco.');
 
 }
 
 function acao3(){
-    alert('Estado foi modificado.')
+    alert('Estado foi modificado.');
 }
