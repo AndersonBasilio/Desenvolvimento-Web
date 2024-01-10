5ec7a5655aa5aa9a5864fc959ce3d231b2c56bae
Anderson Basilio <andersonylde32@gmail.com>
2024-01-10 13:13:04 -0300

    Create script.js

---
+++ curso_javascript/aulas_javascript/javascript_aula009_operador_ternario/script.js
@@ -0,0 +1,42 @@
+//   true &&   true  &&  false  => False
+/*if (2 == 2 && 3 >= 1 && 'a' == 'b'){
+    document.write('Verdadeiro')
+} else {
+    document.write('Falso')
+
+}*/
+
+//   true &&   true  &&  false  => Verdadeiro se uma expressão for verdadeira todas serão
+/*if (2 || 2 || 3 >= 1 || 'a' == 'b'){
+    document.write('Verdadeiro')
+} else {
+    document.write('Falso')
+
+}*/
+
+// false = false
+/*if (!(4 == 2)){
+    document.write('Verdadeiro')
+} else {
+    document.write('Falso')
+
+}*/
+
+var nota = prompt('Digite a nota do aluno: ')
+var faltas = prompt('Digite a quantidade de faltas: ')
+
+var media = 7
+var faltas_maximas = 15
+
+/*if (nota >= media && faltas <= 15){
+    //logica
+    document.write('Aprovado!!')
+
+} else {
+    //logica
+    document.write('Reprovado!!')
+}*/
+
+//var resultado = <condicao> ? <verdadeiro> : <falso>
+var resultado = (nota >= media && faltas <= 15) ? 'Aprovado!!' : 'Reprovado!!'
+document.write(resultado)
