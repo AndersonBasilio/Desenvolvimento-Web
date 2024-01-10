0f6c42e92f37db3912bbfa3a013d48b3d81eb116
Anderson Basilio <andersonylde32@gmail.com>
2024-01-10 13:08:54 -0300

    Create script.js

---
+++ curso_javascript/aulas_javascript/javascript_aula0029_manipulando_classe_elementos/script.js
@@ -0,0 +1,7 @@
+function modificaClasse(){
+    document.getElementById('mensagem').className = 'estilo2';
+}
+
+function desfazerClasse(){
+    document.getElementById('mensagem').className = 'estilo1';
+}
