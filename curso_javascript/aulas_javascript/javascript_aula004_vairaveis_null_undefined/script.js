0e8c1786bb4582047d6a795fd34682637652ce17
Anderson Basilio <andersonylde32@gmail.com>
2024-01-10 13:12:23 -0300

    Create script.js

---
+++ curso_javascript/aulas_javascript/javascript_aula004_vairaveis_null_undefined/script.js
@@ -0,0 +1,15 @@
+        //null ausencia intencional de um valor.
+        var teste = null
+
+        //undefined Esta indefinida
+        var teste2
+
+        console.log(teste)
+        
+        teste = 'Valor modificado com sucesso'
+
+        console.log(teste)
+
+        teste = 127.6
+        
+        console.log(teste)
