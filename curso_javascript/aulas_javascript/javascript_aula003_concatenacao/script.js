7204f5182bd96e421455fa7c51507bf95b665500
Anderson Basilio <andersonylde32@gmail.com>
2024-01-10 13:09:53 -0300

    Create script.js

---
+++ curso_javascript/aulas_javascript/javascript_aula003_concatenacao/script.js
@@ -0,0 +1,3 @@
+var nome = prompt('Digite seu nome: ');
+var idade = prompt('Qual é sua idade? ');
+document.write('<h1>Olá ' + nome +' , tudo bem? Estou vendo aqui que você tem ' + idade + ' anos. </h1>');
