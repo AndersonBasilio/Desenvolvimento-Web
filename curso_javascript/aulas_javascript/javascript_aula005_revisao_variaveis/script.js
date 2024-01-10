f01da477ec5b27a73ba274dab09467dd9d277beb
Anderson Basilio <andersonylde32@gmail.com>
2024-01-10 13:12:45 -0300

    Create script.js

---
+++ curso_javascript/aulas_javascript/javascript_aula005_revisao_variaveis/script.js
@@ -0,0 +1,18 @@
+var a = 10
+var b = 20
+var c = null
+
+document.write('<h1>Valores Normais.</h1>')
+
+document.write('A = ' + a + '<br>')
+document.write('B = ' + b + '<br>')
+document.write('C = ' + c + '<br>')
+
+document.write('<hr>')
+
+document.write('<h1>Invertendo Valores.</h1>')
+
+var c = b
+document.write('A = ' + c + '<br>')
+document.write('B = ' + a + '<br>')
+document.write('C = ' + null + '<br>')
