580313d9e2e364f0d43ebd961b87b8a575dc4a4c
Anderson Basilio <andersonylde32@gmail.com>
2024-01-10 13:12:54 -0300

    Create script.js

---
+++ curso_javascript/aulas_javascript/javascript_aula007_casting/script.js
@@ -0,0 +1,14 @@
+//var variavel1 = prompt('Digite algum numero: ')
+//var variavel2 = prompt('Digite outro numero: ')
+
+var variavel1 = 10
+var variavel2 = 20 
+//variavel1 = parseInt(variavel1)
+//variavel2 = parseFloat(variavel2)
+
+console.log(variavel1)
+console.log(variavel2)
+
+//document.write(variavel1, ' + ' ,  variavel2 , ' = ' , variavel1 + variavel2)
+
+document.write(variavel1.toString() + variavel2.toString())
