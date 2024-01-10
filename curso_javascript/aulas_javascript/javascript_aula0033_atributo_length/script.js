073413bf00d5fd3774f3a18b3d2d348f30c3f8ca
Anderson Basilio <andersonylde32@gmail.com>
2024-01-10 13:09:20 -0300

    Create script.js

---
+++ curso_javascript/aulas_javascript/javascript_aula0033_atributo_length/script.js
@@ -0,0 +1,9 @@
+var lista_frutas = Array(); 
+lista_frutas[5] ='Banana';
+lista_frutas[6] = 'Uva';
+lista_frutas['x'] ='Morango';
+lista_frutas[8] = 'Maçã';
+
+//var lista_frutas = Array('Banana', 'Maçã', 'Morango', 'Uva');
+//var lista_frutas = ['Banana', 'Maçã', 'Morango', 'Uva'];
+console.log(lista_frutas);
