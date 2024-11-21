e07628f6cd177fadce1a2b6fa3e2a7b0a4472075
Anderson Basilio <andersonwylde32@gmail.com>
2024-11-21 08:36:37 -0300

    Exercicio_010 adicionado.

---
+++ curso_javascript/exercicio_javascript/exercicio_010/js/script.js
@@ -0,0 +1,36 @@
+/*
+    Faça um Programa que peça dois números e imprima o maior deles.
+*/
+
+function main(){
+    let [primeiroNumero, segundoNumero] = obterNumeros();
+    let maior =  confereNumeros(primeiroNumero, segundoNumero);
+    document.write(`Maior número digitado foi ${maior}.`)
+}
+
+function obterNumeros(){
+    let primeiroNumero;
+    let segundoNumero;
+    do {
+        primeiroNumero = Number(prompt("Informe o primeiro número: "));
+        segundoNumero = Number(prompt('Informe o segundo número: '));
+        if((isNaN(primeiroNumero) || isNaN(segundoNumero)) || primeiroNumero === 0 || segundoNumero  === 0 || primeiroNumero === segundoNumero){
+            alert('Informe os valores corretamente, não podendo ser iguais ou zero.');
+        }
+    } while((isNaN(primeiroNumero) || isNaN(segundoNumero)) || primeiroNumero === 0 || segundoNumero  === 0 || primeiroNumero === segundoNumero);
+    
+    return [primeiroNumero, segundoNumero];
+}
+
+function confereNumeros(primeiroNumero, segundoNumero){
+    let maior
+    if(primeiroNumero > segundoNumero){
+        maior = primeiroNumero;
+    }else {
+       maior = segundoNumero;
+    }
+    return maior;
+    
+}
+
+main();
