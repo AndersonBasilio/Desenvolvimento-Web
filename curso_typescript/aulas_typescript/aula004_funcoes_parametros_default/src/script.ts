c0b71616350ec269aa5c6daf1ed1920782db3824
Anderson Basilio <andersonwylde32@gmail.com>
2024-11-22 14:35:43 -0300

    Aula004 de Typescript adicionada.

---
+++ curso_typescript/aulas_typescript/aula004_funcoes_parametros_default/src/script.ts
@@ -0,0 +1,6 @@
+function contarHistoria(pesronagem:string='Fubá', atividade:string='Correr no Parque', nome_dono:string = 'Freddie Mercury'){
+    document.write(`Era uma vez um cachorro chamado ${pesronagem}, ele adorava ${atividade},
+         seu dono era o ${nome_dono} e eles viveram felizes para sempre.`);
+}
+
+contarHistoria(undefined, 'Enterrar Ossos', 'Anderson');
