a1e974ef2710dcd056c13b1df1232a7bd2aeacd5
Anderson Basilio <andersonwylde32@gmail.com>
2025-05-26 18:19:27 -0300

    Aula 057 adicionada tipos de dados primitivos

---
+++ curso_javascript/aulas_javascript/javascript_aula057_tipos_dados_primitivos/script.js
@@ -0,0 +1,14 @@
+//Tipos de dados primitivos são string, number, undefined, null, boolean e symbol
+const nome = 'Luiz'; //string
+const nome1 = 'Anderson'; //string
+const nome2 = 'Guanabara'; //string
+const num1 = 10; //number
+const num2 = 10.52; //number
+
+// Usando let 
+let nomeAluno; //undefined -> Não aponta para nenhum local na memória
+const sobrenomeAluno = null; // Nulo -> Não aponta para nenhum local na memória
+const aprovado= true // true ou false
+
+console.log(`O tipo primitivo é ${typeof nome} e o valor é ${nome}`);
+
