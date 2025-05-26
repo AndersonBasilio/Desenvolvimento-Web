6986e6b86debdd04bb07794262a8168958788ab4
Anderson Basilio <andersonwylde32@gmail.com>
2025-05-26 17:47:25 -0300

    Atualização das aulas

---
+++ curso_javascript/aulas_javascript/javascript_aula002_introducao_variaveis/var_let_diferenca/script.js
@@ -0,0 +1,10 @@
+//Com var quando redeclaramos uma variável permiti com let não permite.
+//Com let apresenta um erro e não permite a redeclaração da variável.
+
+/*var nome = 'Anderson';
+var nome = 'Basilio'*/
+
+//Podemos declarar variáveis sem usar let ou var
+//nome = 'Anderson'; //NÃO FAÇA ISSO.
+let nome = 'Anderson'
+console.log(nome);
