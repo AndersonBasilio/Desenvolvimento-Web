57bd30b6fe69d3a33951ae8893e112fe114235fd
Anderson Basilio <andersonwylde32@gmail.com>
2025-05-27 17:47:46 -0300

    Exercicio sobre string adicionado

---
+++ curso_javascript/exercicio_javascript/exercicio_018_string/js/script.js
@@ -0,0 +1,11 @@
+const nome = prompt('Digite seu nome completo: ');
+
+document.body.innerHTML += `Seu nome é: <mark>${nome}</mark>.<br>`;
+document.body.innerHTML += `Seu <mark>${nome}</mark> tem <mark>${nome.replace(' ', ''), nome.length}</mark> letras.<br>`;
+document.body.innerHTML += `A segunda letra do seu nome é:<mark>${nome[1]}</mark> <br>`;
+document.body.innerHTML += `Qual é o primeiro indice da letra <mark>${nome.indexOf('A')} </mark>do seu nome <br>`;
+document.body.innerHTML += `Qual é o ultimo indice da letra (a) do seu nome? <mark>${nome.lastIndexOf('a')}</mark><br>`;
+document.body.innerHTML += `As ultimas 3 letras do seu nome são: <mark>${nome.slice(-3)}</mark><br>`;
+document.body.innerHTML += `As palavras do seu nome são: <mark>${nome.split(' ')}</mark><br>`
+document.body.innerHTML += `Seu nome com letras maiusculas: <mark>${nome.toLocaleUpperCase()}</mark><br>`
+document.body.innerHTML += `Seu nome com letras minusculas: <mark>${nome.toLocaleLowerCase()}</mark><br>`
