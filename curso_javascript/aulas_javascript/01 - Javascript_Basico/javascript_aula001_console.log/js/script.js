04775f2d87acd41f4ac1fca0b0e16b85ffb57281
Anderson Basilio <andersonwylde32@gmail.com>
2025-06-13 19:28:52 -0300

    Organizando pastas das aulas.

---
+++ curso_javascript/aulas_javascript/01 - Javascript_Basico/javascript_aula001_console.log/js/script.js
@@ -0,0 +1,10 @@
+//Usamos a "função para exibir algo na tela."
+//Ponto e virgula é opcional.
+console.log('Olá Mundo!'); // Com aspas simples
+console.log("Olá Mundo!"); // Com aspas duplas
+console.log(`Olá Mundo!`); // Com crase.
+console.log("Olá 'Mundo'") // Se quisermos colocar apas dentro do texto, temos que inverter as aspas.
+
+//Com numeros não precisamos colocar aspas
+//Função console so é exibida no console.
+console.log(35, 15.85, 123.7888888); //Number = Número
---
+++ curso_javascript/aulas_javascript/01 - Javascript_Basico/javascript_aula002_comentarios_codigo/js/script.js
@@ -0,0 +1,11 @@
+// Comentários em Javascript utilizamos (//), dessa forma não ira aparacer e nem ficará com erro.
+console.log('Linha 1');
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+// Ira ignorar esse linha.
+
+//Comentario de bloco é feito dessa forma (/*... */).
+/*
+    Colocamos o comentario e não será exibido.
+*/
