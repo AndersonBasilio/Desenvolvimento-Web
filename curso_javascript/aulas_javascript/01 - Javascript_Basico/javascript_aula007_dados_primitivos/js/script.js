5a10be49528b8be56a5ecc4bcb35ed038fa735e7
Anderson Basilio <andersonwylde32@gmail.com>
2025-06-14 14:40:20 -0300

    Aula 007 adcicionada.

---
+++ curso_javascript/aulas_javascript/01 - Javascript_Basico/javascript_aula007_dados_primitivos/js/script.js
@@ -0,0 +1,24 @@
+// Tipos de dados primitivo são: string, number, undefined e boolean
+
+const nome = "Anderson"; //string - Aspas dupla.
+const nomeBanda = 'Metallica'; // string - Aspas simples.
+const nomeGuitarra = `Les Paul`; // string - Template utiliza crase.
+
+const numero = 10; // Number
+const numFlutuante = 10.59 // Number
+
+// Declarando uma variável, mas não esta sendo inicializada.
+let nomeAluno; // undefined não aponta para local nenhum na memoria.
+
+//Utilizamos null quando queremos indicar para não apontar para nenhum lugar na memoria.
+let sobrenomeAluno = null; // Nulo -> Não aponta para nenhum local na memoria.
+
+// Dados Boolean são um valor logico.
+const aprovado = true; //true ou false;
+
+
+// Para ver o tipo de dados utilizamos typeOf()
+console.log(typeof(aprovado));
+
+
+
