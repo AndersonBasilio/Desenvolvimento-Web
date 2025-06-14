e7d74ea610b10f1fece1662fe9ca6c55988f11e7
Anderson Basilio <andersonwylde32@gmail.com>
2025-06-14 14:10:47 -0300

    Aula 006 adicionanda

---
+++ curso_javascript/aulas_javascript/01 - Javascript_Basico/javascript_aula006_diferenca_var_let/js/script.js
@@ -0,0 +1,8 @@
+// ECMAscript 2015 (ES6) - Algum recurso que foi adicionado na versão ECMAscript é uma padronização do Javascript.
+
+//nome = 'Anderson' // NÃO FAÇA ISSO (irá gerar uma variável golbal.)
+//var nome = 'Anderson Basilio';
+//var nome = 'Metallica'; // Variável foi redeclarada. 
+
+let nome = 'Anderson' // Utilizando let não podemos redeclarar a variável.
+console.log(nome)
