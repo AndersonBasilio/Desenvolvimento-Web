924a5e43e375671750676c6b60838a4ca2abcdee
Anderson Basilio <andersonwylde32@gmail.com>
2025-06-16 13:48:45 -0300

    Adicionando aula012

---
+++ curso_javascript/aulas_javascript/01 - Javascript_Basico/javascript_aula012_objeto_math/js/script.js
@@ -0,0 +1,42 @@
+//Objeto Math disponibilizado pela propria linguagem.
+
+let num1 = 144;
+
+//Dessa forma arredonda para baixo.
+//let num2 = Math.floor(num1);
+
+// Dessa forma arredonda para cima.
+//let num2 = Math.ceil(num1);
+
+// Dessa forma arredonda para o mais proximo, 9.4 arredonda para baixo 9.5 arredonda para cima.
+//let num2 = Math.round(num1);
+
+//Encontra o maior numero dentro da lista.
+//console.log(Math.max(1, 2, 3, 4, 500, -52, 26, 36));
+
+//Encontra o menor numero dentro da lista.
+//console.log(Math.min(1, 2, 3, 4, 500, -52, 26, 36));
+
+// Gera numeros aleatorio.
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+
+//Valor de PI
+//console.log(Math.PI);
+
+//Potenciação
+//console.log(Math.pow(2, 10));
+
+// Potenciação com Operador
+//console.log(2 ** 10);
+
+//Raiz Quadrada console.log(num1  ** (1/2)); ou console.log(num1  ** 0.5);
+//console.log(num1  ** 0.5);
+
+//Em JS podemos dividir um número por ZERO
+//console.log(10 / 0); Ifinity(DADO NUMERICO) significa que o número é infinito, avaliado como true.
+console.log(100 / 0); 
+let umastring = "Um \"texto\" ";
+console.log(umastring ); 
+  
+
+
