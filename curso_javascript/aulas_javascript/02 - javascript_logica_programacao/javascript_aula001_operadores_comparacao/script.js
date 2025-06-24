d6e9fed4004d0c2fcfd66cc26940f3eb53c8fbc7
Anderson Basilio <andersonwylde32@gmail.com>
2025-06-24 18:39:06 -0300

    Adicionando aula001 logica

---
+++ curso_javascript/aulas_javascript/02 - javascript_logica_programacao/javascript_aula001_operadores_comparacao/script.js
@@ -0,0 +1,18 @@
+/* 
+    Operadores de Comparação
+    
+    >   -> Maior que 
+    >=  -> Maior ou igual
+    <   -> Menor
+    <=  -> Menor ou igual
+    ==  -> Igualdade (Checa valor) ( De atribuição) - Não usamos
+    === -> Igualdade restrita (Checa valor e tipo)
+    !=  -> Diferente (Checa valor)
+    !== -> Diferente Restrito (Checa valor e tipo)
+
+
+*/
+const num1 = 10;
+const num2 = '10';
+const comparacao = num1 === num2;
+console.log(comparacao); // Retorna true ou false
