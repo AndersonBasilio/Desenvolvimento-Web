abbdf65cb2f04f8eb601f582e3fb03f37f373b03
Anderson Basilio <andersonwylde32@gmail.com>
2025-08-06 10:26:09 -0300

    adicionando aula 005

---
+++ curso_javascript/aulas_javascript/03 - javascript_funcoes_avancados/javascript_aula005_closure/js/script.js
@@ -0,0 +1,21 @@
+// Closure é a habilidade para acessar o escopo léxico.
+
+// Tem acesso a três escopos, retornaFuncao, função anonima e o global.
+function retornaFuncao(nome){
+    //const nome = 'Anderson';
+
+    // Retornando uma função anonima.
+    return function () {
+       return nome; 
+    };
+}
+
+// Mudando o contexto lexico, chamamos de closure sempre que chamamos a função sera os nomes do parametro.
+const funcao = retornaFuncao('James');
+const funcao2 = retornaFuncao('Hetfield');
+
+// Temos tambem console.dir(funcao);
+console.dir(funcao); // Exibe a função anonima.
+//onsole.log(funcao);
+
+console.log(funcao(), funcao2());
