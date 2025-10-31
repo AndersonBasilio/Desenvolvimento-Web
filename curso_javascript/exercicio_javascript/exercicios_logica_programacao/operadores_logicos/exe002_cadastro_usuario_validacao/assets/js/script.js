16e3652cfacb8b944e40cc9e32e29debe4ec9ef3
Anderson Basilio <andersonwylde32@gmail.com>
2025-10-31 17:37:19 -0300

    Exericicio 002

---
+++ curso_javascript/exercicio_javascript/exercicios_logica_programacao/operadores_logicos/exe002_cadastro_usuario_validacao/assets/js/script.js
@@ -0,0 +1,30 @@
+/*
+    Exercício 2 — Cadastro de usuário com validação
+
+    Crie um programa que solicite os seguintes dados do usuário:
+
+    Nome
+
+    Idade
+
+    Email
+
+    Senha
+
+    Regras para validação:
+
+    O nome não pode ser vazio.
+
+    A idade deve ser entre 18 e 65.
+
+    O email deve conter o símbolo @.
+
+    A senha deve ter pelo menos 8 caracteres.
+
+    Se todas as condições forem atendidas, exiba:
+
+    Cadastro realizado com sucesso!
+
+
+    Caso contrário, exiba uma mensagem específica informando qual regra não foi atendida.
+*/
