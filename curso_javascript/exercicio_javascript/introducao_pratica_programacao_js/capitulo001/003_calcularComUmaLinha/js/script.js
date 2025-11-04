51409ec852b81fb6e4627c3dea4c10d02189dc66
Anderson Basilio <andersonwylde32@gmail.com>
2025-11-04 17:53:45 -0300

    Exercicio 03 adicionado

---
+++ curso_javascript/exercicio_javascript/introducao_pratica_programacao_js/capitulo001/003_calcularComUmaLinha/js/script.js
@@ -0,0 +1,9 @@
+/*
+    Escreva uma linha de código que calcule e imprima o resultado da expressão:
+
+    (14 * 12) / (33 * 144 - 187)
+*/
+
+
+const resultado = (14 * 12) / (33 * 144 - 187);
+console.log(resultado.toFixed(3));
