461a6c6a09958e4367ea6ee2de751a08b1002ea1
Anderson Basilio <andersonwylde32@gmail.com>
2025-11-22 11:01:28 -0300

    Aula 01 capitulo 8 adicionada

---
+++ curso_javascript/aulas_javascript/08 - javascript_tooling_ES6_modules_modulos/javascript_aula001_babel_can_i_use/script.js
@@ -0,0 +1,27 @@
+/*
+    Sites: Can i use -> Para verificar o que é suportado.
+
+    Babel é um tradutor e um compilador de codigos.
+    site é babeljs.io
+
+    Instalação Babel:
+        Abrir Terminal
+
+        Inicializar o Gernciador de pacotes: npm init -y (Irá criar um pakage.json na pasta.)
+
+        Instalar babel: npm install --save-dev @babel/cli @babel/preset-env @babel/core (Instalando como dependencia).
+
+        Converter para um script mais moderno. npx babel script.js -o bundle.js --presets=@babel/present-env
+
+        Aualizar bundle
+            Comando: npx babel script.js -o bundle.js
+
+        Utilizar o bundle.js no arquivo HTML.
+
+
+*/
+
+const nome = 'Anderson';
+const obj = {nome};
+const novoObj = {...obj};
+console.log(novoObj);
