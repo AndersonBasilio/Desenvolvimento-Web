1afb1c44fb62088bed1b55b760596c11c85573e9
Anderson Basilio <andersonwylde32@gmail.com>
2025-11-25 15:46:13 -0300

    Aula 04 Capitulo 8 adicionada.

---
+++ curso_javascript/aulas_javascript/08 - javascript_tooling_ES6_modules_modulos/javascript_aula004_adiciona_css_webpack_modelo/src/index.js
@@ -0,0 +1,24 @@
+/*
+    Estrutura Projeto:
+        pastas:
+            node_modules
+            public (Pasta que o usuário terá acesso)
+                assets
+                    js
+                        bundle.js
+                        bundle.js.map
+                    index.html
+            src
+                assets
+                    css
+                    img
+                    js
+                modules (Pasta modules fica todos os modulos)
+                index.js (Dentro de src)
+            .gitignore
+            package-lock.json
+            package.json
+            webpack.config.js                          
+*/
+
+import './assets/css/style.css' // Importando o css direto no arquivo.
